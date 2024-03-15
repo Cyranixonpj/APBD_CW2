5e1d7ac270a6fbc21b6d2728c877dd4ddaa05a70
Cyranixonpj <s27730@pjwstk.edu.pl>
2024-03-15 18:45:27 +0100

    commit nr2

--- Ćwiczenia_2/Ćwiczenia_2/Program.cs
+++ Ćwiczenia_2/Ćwiczenia_2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Cześć");
+
+
+string[] imiona = { "Olek", "Tomek", "Wiktoria" };
+
+foreach(var a in imiona)
+{
+    Console.WriteLine(a);
+}
